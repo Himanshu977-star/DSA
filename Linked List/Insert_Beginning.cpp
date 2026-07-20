8717fb78cf42674bf19c0cce2f8c59ab4be7794b
Himanshu Dubey <hd15192006@gmail.com>
2026-07-20 10:49:09 +0530

    Add insertion at beginning for linked list
    Implemented insertion of a new node at the beginning of a linked list and displayed the list before and after the insertion.

---
+++ Linked List/Insert_Beginning.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+int main() {
+    Node* head=new Node();
+    Node* second=new Node();
+    Node* third=new Node();
+    
+    head->data=10;
+    second->data=20;
+    third->data=30;
+    
+    head->next=second;
+    second->next=third;
+    third->next=NULL;
+    
+    Node* temp=head;
+    cout<<"Before Insertion at beginning:";
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+    
+    Node* newNode=new Node();
+    newNode->data=5;
+    //Insertion at beginning
+    newNode->next=head;
+    head=newNode;
+    
+    temp=head;
+    cout<<"After Insertion at beginning:";
+    while(temp!=NULL){
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
